3b39786a7907d5a58e7910199aa80b5845fbec3a
Sam Preston <sam.preston@headway.io>
2022-09-30 09:27:59 -0700

    passing build test

--- spec/build.test.js
+++ spec/build.test.js
@@ -1,17 +1,21 @@
-import { execa } from "execa";
+// import { execa } from "execa";
 import * as fs from "fs";
+import { exec } from "child_process";
 
-const cli = (argv = "") => new Promise((resolve, reject) => {
-    const subprocess = execa(`node ./cli.js ${argv}`)
-    subprocess.stdout.pipe(process.stdout)
-    subprocess.stderr.pipe(process.stderr)
-    Promise.resolve(subprocess).then(resolve)
-  });
+
+// const cli = (argv = "") => new Promise((resolve, reject) => {
+//     const subprocess = execa(`node ./cli.js ${argv}`)
+//     subprocess.stdout.pipe(process.stdout)
+//     subprocess.stderr.pipe(process.stderr)
+//     Promise.resolve(subprocess).then(resolve)
+//   });
 
 describe('npm run build', () => {
-    it('outputs a scss file', async() => {
+    it('outputs a scss file',() => {
         // run build
-        await cli('npm run build');
+        // await cli('npm run build');
+
+        exec('npm run build');
 
         // expect file exists
         expect(fs.existsSync('./build/scss/_variables.scss')).toBe(true);
--- spec/cli.js
+++
@@ -1,3 +0,0 @@
-const { program } = require('commander');
-program.command('npm run build').action(() => {});
-program.parse(argv)
