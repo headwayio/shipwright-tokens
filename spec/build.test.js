9e0f0fab0ff6bd3f84f61f97377db5ce2bcf524d
Sam Preston <sam.preston@headway.io>
2022-09-30 11:09:30 -0700

    cleans up changes after running build test

--- spec/build.test.js
+++ spec/build.test.js
@@ -1,23 +1,15 @@
-// import { execa } from "execa";
 import * as fs from "fs";
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
 
 
-// const cli = (argv = "") => new Promise((resolve, reject) => {
-//     const subprocess = execa(`node ./cli.js ${argv}`)
-//     subprocess.stdout.pipe(process.stdout)
-//     subprocess.stderr.pipe(process.stderr)
-//     Promise.resolve(subprocess).then(resolve)
-//   });
+describe("npm run build", () => {
+  afterEach(() => {
+    exec("git restore ./build ./tokens");
+  });
 
-describe('npm run build', () => {
-    it('outputs a scss file',() => {
-        // run build
-        // await cli('npm run build');
+  it("outputs a scss file", async () => {
+    execSync("npm run build");
 
-        exec('npm run build');
-
-        // expect file exists
-        expect(fs.existsSync('./build/scss/_variables.scss')).toBe(true);
-    });
+    expect(fs.existsSync("./build/scss/_variables.scss")).toBe(true);
+  });
 });
