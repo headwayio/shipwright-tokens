2f4ae513cfc177387c89da9b64b2b1761f0fd9a2
Sam Preston <sam.preston@headway.io>
2022-10-14 11:06:21 -0700

    generate configr file with CLI input for style dictionary inputs/outputs

---
+++ build-config.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+
+const buildConfig = (dist = "build", src = "tokens") => {
+  const config = {
+    source: [`${src}/**/*.json`],
+    platforms: {
+      tailwind: {
+        transformGroup: "js/custom",
+        buildPath: `./${dist}/`,
+        files: [
+          {
+            destination: "typography.json",
+            format: "twTypography",
+          },
+          {
+            destination: "misc.json",
+            format: "twMisc",
+          },
+          {
+            destination: "colors.json",
+            format: "twColors",
+          },
+          {
+            destination: "shadows.json",
+            format: "twShadows",
+          },
+        ],
+      },
+    },
+  };
+
+  fs.writeFile("config.json", JSON.stringify(config), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
+
+buildConfig(args[0], args[1]);
--- spec/build.test.js
+++ spec/build.test.js
@@ -2,14 +2,16 @@ const fs = require("fs");
 const { exec, execSync } = require("child_process");
 
 
-describe("npm run build", () => {
-  it("outputs a scss file", async () => {
-    exec("rm -rf ./build");
+describe("shipwright tokens action", () => {
+  it("outputs style files", async () => {
+    exec("rm -rf build/ tokens/transformed config.json");
+
+    execSync("node build-config.js build/tailwind tokens/transformed");
     execSync("npx token-transformer tokens/raw/tokens.json tokens/transformed/tokens.json");
     execSync("node build.js");
 
     expect(fs.existsSync("./build/tailwind/colors.json")).toBe(true);
 
-    exec("rm -rf build/ tokens/transformed");
+    exec("rm -rf build/ tokens/transformed config.json");
   });
 });
