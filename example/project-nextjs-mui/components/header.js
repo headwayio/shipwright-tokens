0e49a7de365a6ece1f6066acc99749b8a7c469bf
Sam Preston <sam.preston@headway.io>
2022-10-31 11:45:20 -0700

    added example project with nextjs & mui

---
+++ example/project-nextjs-mui/components/header.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { Typography } from "@mui/material";
+import Container from "./contaner";
+import Logo from "../public/images/headway.png";
+
+const Header = () => {
+  return (
+    <Container>
+      <Image
+        objectFit="cover"
+        src={Logo}
+        alt="Make Waves"
+        layout="fill"
+        priority
+      />
+      <Typography variant="h4-regular" sx={{ color: "type.black.secondary" }}>
+        Shipwright Tokens - Next JS / MUI
+      </Typography>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ example/project-nextjs-mui/pages/index.js
@@ -0,0 +1,13 @@
+import Colors from "../components/colors"
+import Header from "../components/header"
+import Type from "../components/typography"
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Type />
+      <Colors />
+    </div>
+  )
+}
