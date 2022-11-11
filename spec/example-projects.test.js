3101faf82b5dbc4699fd66f3baa5768e25e54942
Sam Preston <sam.preston@headway.io>
2022-11-11 11:34:13 -0800

    test comparing example project theme configs to provided theme templates

---
+++ spec/example-projects.test.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+describe("example projects", () => {
+    const tailwindThemeTemplate = fs.readFileSync("./theme_templates/tailwind.config.js", "utf8");
+    const muiThemeTemplate = fs.readFileSync("./theme_templates/muiTheme.js", "utf8");
+
+    describe("next / tailwind", () => {
+        it("theme config matches provided theme template", () => {
+            const themeConfig = fs.readFileSync("./example/project-nextjs-tailwind/styles/tailwind/tailwind.config.js", "utf8");
+            expect(themeConfig).toEqual(tailwindThemeTemplate);
+        });
+    });
+
+    describe("next / mui", () => {
+        it("theme config matches provided theme template", () => {
+            const themeConfig = fs.readFileSync("./example/project-nextjs-mui/styles/mui/muiTheme.js", "utf8");
+            expect(themeConfig).toEqual(muiThemeTemplate);
+        });
+    });
+
+    describe("elixir / tailwind", () => {
+        it("theme config matches provided theme template", () => {
+            const themeConfig = fs.readFileSync("./example/project-elixir-tailwind/assets/styles/tailwind/tailwind.config.js", "utf8");
+            expect(themeConfig).toEqual(tailwindThemeTemplate);
+        });
+    });
+});
