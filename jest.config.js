f627c0589920f5a19eb6932d068be2b7f9069cbb
Ganzfriend <melissaganzfried@gmail.com>
2022-11-16 18:29:56 +0100

    Update preset in jest config to transform to esm instead of cjs

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.json",
+    },
+  },
 };
