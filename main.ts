95d62951f2981c0fef921266542e0cab7b596b56
Sam Preston <sam.preston@headway.io>
2022-11-21 11:50:07 -0800

    moved shipwrightScript from main.ts to action-scripts/shipwright-script.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,5 @@
-import { execSync } from "child_process";
-import copyThemeFile from "./action-scripts/copy-theme.js";
-import { buildConfig, StyleSystems } from "./action-scripts/build-config.js";
-
-type Props = {
-  figmaTokenFile: string;
-  outputFolder: string;
-  actionPath?: string;
-  styleSystem: StyleSystems;
-};
-
-const shipwrightScript = async ({
-  figmaTokenFile = "token/token.json",
-  outputFolder = "build",
-  styleSystem = StyleSystems.Mui,
-  actionPath = "./",
-}: Props) => {
-  const transformedTokenPath = `${outputFolder}/style-dictionary/tokens.json`;
-
-  // transform figma token file to style-dictionary ready JSON
-  execSync(
-    `npx token-transformer ${figmaTokenFile} ${transformedTokenPath} --expandTypography=true`
-  );
-
-  // create the style-dictionary build config based on action config
-  buildConfig({ transformedTokenPath, outputFolder, styleSystem });
-
-  // execute the style-dictionary transformations
-  import("./action-scripts/build.js").then((module) => {
-    const build = module.default;
-    build();
-  });
-
-  // copy themeFile
-  copyThemeFile({ outputFolder, styleSystem, actionPath });
-};
+import { shipwrightScript } from "action-scripts/shipwright-script";
+import { StyleSystems } from "action-scripts/build-config";
 
 // script execution
 const [figmaTokenFile, outputFolder, styleSystem, actionPath] =
@@ -45,5 +11,3 @@ shipwrightScript({
   styleSystem: styleSystem as StyleSystems,
   actionPath,
 });
-
-export { shipwrightScript };
