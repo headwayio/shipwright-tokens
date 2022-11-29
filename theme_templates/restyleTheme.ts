5af9f49634f7501b69d28b2b8a16f590235e95e7
Sam Preston <sam.preston@headway.io>
2022-11-29 08:01:57 -0800

    updated restyle theme template

--- theme_templates/restyleTheme.ts
+++ theme_templates/restyleTheme.ts
@@ -7,14 +7,50 @@ const misc = require("./misc");
 
 const theme = createTheme({
   colors: {
-    // Extend this as needed in your own theme
-    mainBackground: palette.background.white.primary,
-    mainForeground: palette.type.black.primary,
-    cardBackground: palette.background.white.secondary,
-    cardForeground: palette.type.black.secondary,
+    primaryMain: palette.primary.main,
+    primaryDark: palette.primary.dark,
+    primaryLight: palette.primary.light,
+    secondaryMain: palette.secondary.main,
+    secondaryDark: palette.secondary.dark,
+    secondaryLight: palette.secondary.light,
+    greyMain: palette.grey.main,
+    greyDark: palette.grey.dark,
+    greyLight: palette.grey.light,
+    warningMain: palette.warning.main,
+    warningDark: palette.warning.dark,
+    warningLight: palette.warning.light,
+    infoMain: palette.info.main,
+    infoDark: palette.info.dark,
+    infoLight: palette.info.light,
+    successMain: palette.success.main,
+    successDark: palette.success.dark,
+    successLight: palette.success.light,
+    errorMain: palette.error.main,
+    errorDark: palette.error.dark,
+    errorLight: palette.error.light,
+    typeBlackPrimary: palette.type.black.primary,
+    typeBlackSecondary: palette.type.black.secondary,
+    typeBlackTertiary: palette.type.black.tertairy,
+    typeBlackDeactivated: palette.type.black.deactivated,
+    typeWhitePrimary: palette.type.white.primary,
+    typeWhiteSecondary: palette.type.white.secondary,
+    typeWhiteTertiary: palette.type.white.tertiary,
+    typeWhiteDeactivated: palette.type.white.deactivated,
+    buttonActive: palette.button.active,
+    buttonHover: palette.button.hover,
+    buttonSelected: palette.button.selected,
+    buttonDeactivated: palette.button.deactivated,
+    buttonDeactivatedBackground: palette.button.deactivatedBackground,
+    backgroundWhiteBase: palette.background.white.base,
+    backgroundWhitePrimary: palette.background.white.primary,
+    backgroundWhiteSecondary: palette.background.white.secondary,
+    backgroundWhiteLine: palette.background.white.line,
+    backgroundBlackBase: palette.background.black.base,
+    backgroundBlackPrimary: palette.background.black.primary,
+    backgroundBlackSecondary: palette.background.black.secondary,
+    backgroundBlackLine: palette.background.black.line,
   },
   spacing: {
-    // Extend this as needed in your own theme
     s: 8,
     m: 16,
     l: 24,
@@ -22,7 +58,6 @@ const theme = createTheme({
     xxl: 48,
   },
   breakpoints: {
-    // Extend this as needed in your own theme
     phone: 0,
     tablet: 600,
     web: 905,
@@ -30,24 +65,13 @@ const theme = createTheme({
     desktop: 1440,
   },
   shadows: {
-    // Pull from this in your own theme as needed
     ...shadows,
   },
   misc: {
-    // Pull from this in your own theme as needed
     ...misc,
   },
   textVariants: {
     ...typography,
-    // defaults: {
-    //   // same as body
-    //   fontFamily: style_dictionary.fontFamiliesInter,
-    //   fontSize: {
-    //     phone: style_dictionary.fontSize5,
-    //     tablet: style_dictionary.fontSize5,
-    //   },
-    //   lineHeight: style_dictionary.lineHeights5,
-    // },
   },
 });
 
