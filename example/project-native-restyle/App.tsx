eb2209aff63105cc3b3a226829bef180a58c6eeb
Jorge Rodriguez <jorge@headway.io>
2023-01-31 14:17:27 -0500

    Load Inter font using hook from expo.

--- example/project-native-restyle/App.tsx
+++ example/project-native-restyle/App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, ScrollView, View } from "react-native";
+import { StyleSheet, ScrollView, View, Text } from "react-native";
 import { createBox, createText, ThemeProvider } from "@shopify/restyle";
 import theme from "./theme";
 import Header from "./components/header";
 import Typography from "./components/typography";
 import Colors from "./components/colors";
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Inter': require('./assets/fonts/Inter/Inter-Black.otf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  
   console.log(theme.textVariants.ios.largeTitle);
   console.log(theme.textVariants.largeTitle);
   return (
