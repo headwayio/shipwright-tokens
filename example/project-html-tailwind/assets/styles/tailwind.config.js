66c45b55dfb491ad1990a8e75aad739310b5f50e
Sam Preston <sam.preston@headway.io>
2023-02-28 10:27:11 -0800

    Example Project: HTML / Tailwind (#67)
    * added example project html / tailwind
    
    * added tests for example/project-html-tailwind
    
    * added html-tailwind test to 'prepush-if-changed'

---
+++ example/project-html-tailwind/assets/styles/tailwind.config.js
@@ -0,0 +1,32 @@
+const plugin = require("tailwindcss/plugin");
+const colors = require("./colors");
+const shadows = require("./shadows");
+const typography = require("./typography");
+const misc = require("./misc");
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors,
+      shadows,
+      ...misc,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ...typography,
+      });
+    }),
+  ],
+  mode: "jit",
+};
---
+++ example/project-html-tailwind/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  presets: [
+    require('./assets/styles/tailwind.config.js')
+  ],
+}
