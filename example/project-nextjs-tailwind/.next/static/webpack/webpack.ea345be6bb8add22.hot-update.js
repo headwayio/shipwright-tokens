e5e24689c1edc3ef06bf511f6880fea8f666876a
Sam Preston <sam.preston@headway.io>
2023-03-13 09:41:36 -0700

    moved existing components into new component file structure

--- example/project-nextjs-tailwind/.next/static/webpack/webpack.ea345be6bb8add22.hot-update.js
+++ example/project-nextjs-tailwind/.next/static/webpack/webpack.ea345be6bb8add22.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "65d2f64f162bcfbc"; }
+/******/ 	__webpack_require__.h = function() { return "ec277b96eba19e07"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
