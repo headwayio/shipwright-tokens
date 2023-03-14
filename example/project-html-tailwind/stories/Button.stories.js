e643ab75056f2aefffa0b57984fd5050474d1551
Sam Preston <sam.preston@headway.io>
2023-03-13 17:29:37 -0700

    adde sstorybook and components/stories for button, input, card

---
+++ example/project-html-tailwind/stories/Button.stories.js
@@ -0,0 +1,16 @@
+import Filled from "../components/button/filled.html";
+import Outlined from "../components/button/outlined.html";
+import Ghosted from "../components/button/ghost.html";
+import Icons from "../components/button/icon.html";
+
+export default {
+    title: "Components/Button"
+};
+
+export const Fill = () => Filled;
+
+export const Outline = () => Outlined;
+
+export const Ghost = () => Ghosted;
+
+export const Icon = () => Icons;
--- example/project-html-tailwind/tailwind.config.js
+++ example/project-html-tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./components/**/*.{html, js}"],
   presets: [
     require('./assets/styles/tailwind.config.js')
   ],
