e2ed9ab236242d420b87b44ebd392bd9302cf01b
Sam Preston <sam.preston@headway.io>
2023-03-14 09:56:00 -0700

    Beef up example project Twig / Tailwind (#102)
    * added necessary deps
    
    * moved existing components into new component file structure
    
    * added button, input & card components
    
    * added stories for existing components
    
    * updated tokens & typography token component
    
    * updated documentation
    
    * updated theme_templates/tailwind.config.js to correctly handle shadows
    
    * updated tailsind config in example project to match
    
    * updated tailwind config in elixir & web components projects to match new exmaple
    
    * updated tailwind config in all other tailwind example projects
    
    * added necessary deps
    
    * added components/stories for atom/button, atom/input, organism/card, atom/iconography

--- example/project-twig-tailwind/fiveserver.config.js
+++
@@ -1,4 +0,0 @@
-// fiveserver.config.js
-module.exports = {
-  php: "/opt/homebrew/bin/php",
-};
---
+++ example/project-twig-tailwind/stories/Typography.stories.js
@@ -0,0 +1,10 @@
+import Typography from "../templates/shipwright/components/tokens/typography.html.twig";
+
+export default {
+    title: "Tokens/Typography",
+    argTypes: {}
+};
+
+const Template = (args) => Typography({...args});
+
+export const Type = Template.bind({});
