a26cfed5e0c31faaa2449d57ed35d0bbab0f3720
Thomas Kay <tomxkay@pm.me>
2023-03-28 13:35:56 -0400

    Remove bloat variants

--- example/project-rails-tailwind/test/components/stories/input_component_stories.rb
+++ example/project-rails-tailwind/test/components/stories/input_component_stories.rb
@@ -6,7 +6,7 @@ class InputComponentStories < ViewComponent::Storybook::Stories
                   description: 'display arrow at end of input. available if `variant` is `input`.'
   control :error_message, as: :text,
                           description: 'error message for input.message informing user of invalid input.'
-  control :icon, as: :select, options: %w[search credit-card mail phone user],
+  control :icon, as: :select, options: %w[search],
                  description: 'feather icon name to place on left-hand side of input. you can use any feather icon name in this field, but the most common have been added as options to storybook. available if `variant` is `input`.'
   control :name, as: :text, description: 'form input name.'
   control :placeholder, as: :text, description: 'attribute for placeholder that appears within input.'
@@ -18,7 +18,7 @@ class InputComponentStories < ViewComponent::Storybook::Stories
                  description: 'form input type (i.e. email, password, text, etc.). available if `variant` is `input`.'
   control :value, as: :text,
                   description: 'value of the input. useful for filling in form with previously submitted fields.'
-  control :variant, as: :select, options: %w[input textarea],
+  control :variant, as: :select, options: %w[input],
                     description: 'either input field, select, or textarea.'
   def input(disabled: false, arrow: false, error_message: '', icon: 'search', name: '', placeholder: 'Placeholder Text',
             required: false, rows: '4', success_message: '', type: 'text', value: '', variant: 'input')
